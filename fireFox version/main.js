c369f00e7cf0f1b9f6a064b3b8b49a65f5b70a4b
shengxinjun <498740190@qq.com>
2020-07-15 14:11:53 +0800

    fireFox version

---
+++ fireFox version/main.js
@@ -0,0 +1,46 @@
+$(function() {
+	function onCreated(windowInfo) {
+  console.log(`Created window: ${windowInfo.id}`);
+}
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+browser.browserAction.onClicked.addListener((tab) => {
+  var creating = browser.windows.create({
+    url: ["https://developer.mozilla.org",
+          "https://addons.mozilla.org"]
+  });
+  creating.then(onCreated, onError);
+});
+	
+	$("input").focus(function(){
+		document.getElementById("tip").style.display="none";
+	}) 
+	$("#submit").click(function(e){
+		var tkl = $("#talking").val();
+		 $.ajax({
+            url: "http://test.shengxinjun.top/tkl",
+			type : 'GET',
+			dataType : 'json',
+			data : {
+				tkl:tkl
+			},
+		}).done(function(e) {
+				var data =  JSON.parse(e.message);
+				if(data.code==200){
+					document.getElementById("lj").href=data.data.click_url;
+					document.getElementById("lj").style.display='block';
+				}else{
+					document.getElementById("tip").style.display="inline";
+				}
+		}).fail(function(e) {
+		});
+		
+	}); 
+	$("#lj").click(function(e){	
+		document.getElementById("lj").style.display='none';
+		}); 	
+	 
+});
